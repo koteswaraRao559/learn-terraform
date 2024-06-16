d1c428fa6623ed864fe77603a7c687ba4f1addd3
koteswaraRao559 <koteswararao1999@gmail.com>
2024-06-16 11:23:48 +0530

    4th commit

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -6,11 +6,24 @@ output "sample" {
   value = var.sample
 }
 
+#List of variables
 variable "fruits" {
   default = ["apple","mango","banana"]
 }
 
-output "fruits" {
+output "fruits_with_firstfruit" {
   value = var.fruits[0]
-  value1 = var.fruits[1]
+}
+
+#Map variables
+variable "fruits_with_quantity" {
+  default = {
+    apple = 100,
+    mango = 200,
+    banana = 300
+  }
+}
+
+output "fruits_apple_quantity" {
+  value = var.fruits_with_quantity["apple"]
 }
