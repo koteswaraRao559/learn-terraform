9c965f7199d7680a8e5f380f97f73e5f61def24c
koteswaraRao559 <koteswararao1999@gmail.com>
2024-06-17 11:47:57 +0530

    4th commit

---
+++ expense-v3/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "instances" {
+  for_each = var.components
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = var.sg_id
+  tags = {
+    Name = "${each.value["name"]}-${var.env}"
+  }
+}
+#resource "aws_route53_record" "frontend" {
+#  zone_id = var.zone_id
+#  name    = "frontend-${var.env}"
+#  type    = "A"
+#  ttl     = 300
+#  records = [aws_instance.frontend.private_ip]
+#}
+
