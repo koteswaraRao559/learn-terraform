4dbed9eac70bc5e5dd48219528945221b2122be1
koteswaraRao559 <koteswararao1999@gmail.com>
2024-07-01 17:21:00 +0530

    4th commit

--- 06-data-sources/main.tf
+++ 06-data-sources/main.tf
@@ -1,7 +1,18 @@
-data "aws_ssm_parameter" "user" {
-  name = "dev.rds.username"
+#data "aws_ssm_parameter" "user" {
+#  name = "dev.rds.username"
+#}
+#
+#output "username" {
+#  value = data.aws_ssm_parameter.user.value
+#}
+
+data "aws_instance" "instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["workstation"]
+  }
 }
 
-output "username" {
-  value = data.aws_ssm_parameter.user.value
+output "instance" {
+  value = "instance ID - ${data.aws_instance.instance.id} | Public Ip - ${data.aws_instance.instance.public_ip}"
 }
